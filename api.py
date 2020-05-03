3511af4b5706c402f7a226bad470182a5de4770a
Andrii Nemchenko <a.nemchenko@icloud.com>
2020-05-03 20:54:42 +0300

    2

---
+++ api.py
@@ -0,0 +1,26 @@
+import json
+import requests
+from bitsv.network.meta import Unspent
+
+def get_unspents(address):
+  r = requests.get("https://bchsvexplorer.com/api/addr/{}/utxo".format(address), timeout=30)
+  r.raise_for_status()
+  utxos = [
+    Unspent(amount=utxo['satoshis'],
+            confirmations=utxo['confirmations'],
+            txid=utxo['txid'],
+            txindex=utxo['vout'])
+    for utxo in r.json()
+  ]
+  return sorted(utxos, key=lambda utxo: (-utxo.confirmations, utxo.amount))
+
+def send(tx_hex):
+  r = requests.post(
+    'https://api.whatsonchain.com/v1/bsv/main/tx/raw',
+    data=json.dumps({'txhex': tx_hex}),
+    headers={
+      'Content-Type': 'application/json'
+    },
+  )
+  r.raise_for_status()
+  return r.text
